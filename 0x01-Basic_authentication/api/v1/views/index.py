c3fa0efd7d502d238ccef20043acb28177d9e5a1
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-08-05 20:10:50 +0000

    new commit

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,7 +32,7 @@ def unauthorized() -> str:
     Return:
       - 401 error
     """
-    return abort(401)
+    abort(401)
 
 
 @app_views.route('/forbidden', strict_slashes=False)
@@ -41,4 +41,4 @@ def forbidden() -> str:
     Return:
       - 403 error
     """
-    return abort(403)
+    abort(403)
