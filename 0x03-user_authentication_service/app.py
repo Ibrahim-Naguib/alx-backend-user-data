8065e975c692bcda2c334adb31b73ddb85d2a930
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-08-14 15:00:11 +0000

    new commit

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,17 +73,18 @@ def reset_password() -> str:
     return jsonify({"email": f"{email}", "reset_token": f"{reset_token}"}), 200
 
 
-@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> str:
-    email = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_password = request.form.get('new_password')
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+
     try:
         AUTH.update_password(reset_token, new_password)
     except ValueError:
         abort(403)
 
-    return jsonify({"email": f"{email}", "message": "Password updated"}), 200
+    return jsonify({"email": f"{email}", "message": "Password updated"})
 
 
 if __name__ == "__main__":
